61da26d60829f275cbede933a782595f40b8b67a
DanielHenryL <lagunasdaniel001@gmail.com>
2023-11-20 11:29:27 -0500

    Crear schema y modelos de pokemon

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,2 +1,16 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type CatDocument = HydratedDocument<Pokemon>;
 // Un entities se entiende el como se veria la base de datos.('la tabla').
-export class Pokemon {}
+@Schema()
+export class Pokemon {
+
+    @Prop({ unique: true, index: true })
+    name:string;
+
+    @Prop({ unique: true, index: true })
+    no:number;
+
+}
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
