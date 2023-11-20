216f0f35dab9bc414d4306299e613e2dcddc27c2
DanielHenryL <lagunasdaniel001@gmail.com>
2023-11-20 12:13:04 -0500

    Validar de forma global los campos y validar los campos, ver resultado  reflejado en el POST

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,2 +1,13 @@
+import { IsString, MinLength, IsInt, IsPositive, Min } from "class-validator";
+
 // El Dto se ve como lo la data o campos que el frontend o usuario debe enviarnos
-export class CreatePokemonDto {}
+export class CreatePokemonDto {
+    @MinLength(1)
+    @IsString()
+    name: string;
+
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+}
